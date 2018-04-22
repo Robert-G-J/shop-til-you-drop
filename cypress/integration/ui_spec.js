c4a024b209a7aa6655d8e49cf70805d6521dbea2
Robert Jones <robert.g.jones@gmail.com>
2018-04-22 17:33:52 +0100

    Update UI tests

--- cypress/integration/ui_spec.js
+++ cypress/integration/ui_spec.js
@@ -16,9 +16,8 @@ describe("Shopping Cart", () => {
     });
 
     it("has DOM elements of", () => {
-      cy.get(".header__title").should("contain", "Shopping Cart");
+      cy.get(".header__title").should("contain", "Rick's Kit Cart");
       cy.get("tr").within(() => {
-        cy.get("td.id").should("not.be", "visible");
         cy.get("td.product");
         cy.get("td.product > .description");
         cy.get("input.quantity");
